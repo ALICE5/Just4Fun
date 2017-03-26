e38ab2510dc4c65f7a6d37835a93b170cb3dca22
Yan <alice4u@bjtu.edu.cn>
2017-03-26 23:30:40 +0800

    Project Setup

---
+++ Air Tunnel Walk/GameplayScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameplayScene.swift
+//  Air Tunnel Walk
+//
+//  Created by ALICE on 2017/3/26.
+//  Copyright © 2017年 42fun. All rights reserved.
+//
+
+import SpriteKit
+
+class GameplayScene:SKScene{
+    
+    override func didMove(to view: SKView) {
+
+    }
+
+
+
+} // class
